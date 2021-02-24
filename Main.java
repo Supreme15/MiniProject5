0a7355eaea46d25953f6661fd260d3990fb2d30d
Darrel Supreme <darrel.supreme@patelhs.org>
2021-02-24 23:30:52 +0000

    made method and scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
-import util.java.Scanner;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+
+    System.out.println("How musicians are in your group?");
+    int memb = scan.nextInt();
+    int newOrder = members(memb);
+    System.out.println("Please divide your group into " + newOrder + " in each row");
   }
 
   //
+  public static int members(int memb)
+  {
+    if(memb == 2)
+    {
+       return 0;
+       
+    }else{ 
+     return memb % members(memb - 1);
+    }
+  }
 }
